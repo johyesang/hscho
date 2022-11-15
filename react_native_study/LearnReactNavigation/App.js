5a6f22263af3aebfdd39dc04eb3fc1ed58830e52
johyesang <hscho@hempharma.bio>
2022-11-16 00:29:41 +0900

    Add New React Project for Studing Navigation

--- react_native_study/LearnReactNavigation/App.js
+++ react_native_study/LearnReactNavigation/App.js
@@ -3,22 +3,59 @@ import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import HomeScreen from './screens/HomeScreen';
 import DetailScreen from './screens/DetailScreen';
-import {View, Text} from 'react-native';
+import HeaderlessScreen from './screens/HeaderlessScreen';
+import {TouchableOpacity, View, Text} from 'react-native';
 
 const Stack = createNativeStackNavigator();
 
 function App() {
   return (
-    /*    <NavigationContainer>
-      <Stack.Navigator inintialRouteName="Home">
-        <Stack.Screen name="Home" component={HomeScreen} />
-        <Stack.Screen name="Detail" component={DetailScreen} />
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home" screenOptions={{headerShown: false,}}>
+        <Stack.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{
+            title: '홈',
+            headerStyle: {
+              backgroundColor: '#29b6f6',
+            },
+            headerTintColor: '#ffffff',
+            headerTitleStyle: {
+              fontWeight: 'bold',
+              fontSize: 20,
+            },
+          }}
+        />
+        <Stack.Screen
+          name="Detail"
+          component={DetailScreen}
+          options={{
+            headerBackVisible: false,
+            headerLeft: ({onPress}) => (
+              <TouchableOpacity onPress={onPress}>
+                <Text>Left</Text>
+              </TouchableOpacity>
+            ),
+            headerTitle: ({children}) => (
+              <View>
+                <Text>{children}</Text>
+              </View>
+            ),
+            headerRight: () => (
+              <View>
+                <Text>Right</Text>
+              </View>
+            ),
+          }}
+        />
+        <Stack.Screen
+          name="Headerless"
+          component={HeaderlessScreen}
+          options={{headerShown: false}}
+        />
       </Stack.Navigator>
     </NavigationContainer>
-    */
-    <View>
-      <Text> for test</Text>
-    </View>
   );
 }
 
--- react_native_study/LearnReactNavigation/screens/DetailScreen.js
+++ react_native_study/LearnReactNavigation/screens/DetailScreen.js
@@ -1,13 +1,40 @@
-import React from 'react';
-import {View, Button, Text} from 'reactiv-native';
+import React, {useEffect} from 'react';
+import {View, Text, StyleSheet, Button} from 'react-native';
 
-function DetailScreen({navigation}) {
+function DetailScreen({route, navigation}) {
+  useEffect(() => {
+    navigation.setOptions({
+      title: `상세 정보- ${route.params.id}`,
+    });
+  }, [navigation, route.params.id]);
   return (
-    <View>
-      <Text>Detail</Text>
-      <Button title="Detail 열기" onPress={() => navigation.navigate('Home')} />
+    <View style={styles.block}>
+      <Text style={styles.text}>id: {route.params.id} </Text>
+
+      <View style={styles.buttons}>
+        <Button
+          title="다음"
+          onPress={() => navigation.push('Detail', {id: route.params.id + 1})}
+        />
+        <Button title="뒤로가기" onPress={() => navigation.pop()} />
+        <Button title="처음으로 가기" onPress={() => navigation.popToTop()} />
+      </View>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  block: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text: {
+    fontSize: 48,
+  },
+  buttons: {
+    flexDirection: 'row',
+  },
+});
 
 export default DetailScreen;
